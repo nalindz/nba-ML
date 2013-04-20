6cd70b112faef5feb60d1271eaccdf0c3628a091
Nalin <nalindz@gmail.com>
2013-04-20 14:24:42 -0700

    added ranges

--- nba-nn.py
+++ nba-nn.py
@@ -4,10 +4,26 @@ import csv
 
 cr = csv.reader(open("master_list_no_name.csv","rb"))
 inputs = []
-outputs = []
+targets = []
 for row in cr:
-    input.append(row[:-1])
-    outputs.append(row[-1:])
+    inputs.append(row[:-1])
+    targets.append(row[-1:])
 
-print input
-print outputs
+inputs = inputs[1:]
+targets = targets[1:]
+
+num_nodes = 5
+net = neurolab.net.newff([[1, 40000], 
+    [1, 20000],
+    [1, 16000], 
+    [1, 4000], 
+    [1, 4000], 
+    [1, 3000], 
+    [60, 95], 
+    [0, 12], 
+    [0, 20], 
+    [0, 10], 
+    [0, 10]],
+    [num_nodes, 1])
+
+err = net.train(inputs, targets, show=4)
